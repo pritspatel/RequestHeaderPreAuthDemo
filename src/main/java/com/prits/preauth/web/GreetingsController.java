87035674f42342ab2604dfe7cf13465a4a6f0627
Pritesh Patel <3ppatel@gmail.com>
2017-02-25 09:48:50 -0500

    -- added auth check in api

--- src/main/java/com/prits/preauth/web/GreetingsController.java
+++ src/main/java/com/prits/preauth/web/GreetingsController.java
@@ -1,7 +1,9 @@
 package com.prits.preauth.web;
 
 import com.prits.preauth.entity.Greeting;
-import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,9 +19,16 @@ public class GreetingsController {
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET, produces = APPLICATION_JSON_VALUE)
     public @ResponseBody Greeting sayHello(){
-        Greeting g = new Greeting();
-        g.setId(11111L);
-        g.setMessage("Hello From Spring Security Framework");
-        return g;
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth.isAuthenticated()) {
+            Greeting g = new Greeting();
+            g.setId(11111L);
+            g.setMessage("Hello From Spring Security Framework");
+            return g;
+        } else {
+            throw new AccessDeniedException("User is not authenticated!!!");
+        }
+
+
     }
 }
